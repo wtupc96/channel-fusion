59176ab6806efbcc3cf15d60f009740e886fd466
Peter_Chan <wtupc96@users.noreply.github.com>
2019-12-25 17:54:59 +0800

    init

---
+++ dataloader.py
@@ -0,0 +1,377 @@
+from __future__ import print_function
+
+import os.path
+import random
+import sys
+
+import numpy as np
+from PIL import Image
+
+if sys.version_info[0] == 2:
+    import cPickle as pickle
+else:
+    import pickle
+import os
+import torch
+import torch.utils.data as data
+
+
+class VisionDataset(data.Dataset):
+    _repr_indent = 4
+
+    def __init__(self, root, transforms=None, transform=None, target_transform=None):
+        if isinstance(root, torch._six.string_classes):
+            root = os.path.expanduser(root)
+        self.root = root
+
+        has_transforms = transforms is not None
+        has_separate_transform = transform is not None or target_transform is not None
+        if has_transforms and has_separate_transform:
+            raise ValueError("Only transforms or transform/target_transform can "
+                             "be passed as argument")
+
+        # for backwards-compatibility
+        self.transform = transform
+        self.target_transform = target_transform
+
+        if has_separate_transform:
+            transforms = StandardTransform(transform, target_transform)
+        self.transforms = transforms
+
+    def __getitem__(self, index):
+        raise NotImplementedError
+
+    def __len__(self):
+        raise NotImplementedError
+
+    def __repr__(self):
+        head = "Dataset " + self.__class__.__name__
+        body = ["Number of datapoints: {}".format(self.__len__())]
+        if self.root is not None:
+            body.append("Root location: {}".format(self.root))
+        body += self.extra_repr().splitlines()
+        if hasattr(self, "transforms") and self.transforms is not None:
+            body += [repr(self.transforms)]
+        lines = [head] + [" " * self._repr_indent + line for line in body]
+        return '\n'.join(lines)
+
+    def _format_transform_repr(self, transform, head):
+        lines = transform.__repr__().splitlines()
+        return (["{}{}".format(head, lines[0])] +
+                ["{}{}".format(" " * len(head), line) for line in lines[1:]])
+
+    def extra_repr(self):
+        return ""
+
+
+class StandardTransform(object):
+    def __init__(self, transform=None, target_transform=None):
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __call__(self, input, target):
+        if self.transform is not None:
+            input = self.transform(input)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        return input, target
+
+    def _format_transform_repr(self, transform, head):
+        lines = transform.__repr__().splitlines()
+        return (["{}{}".format(head, lines[0])] +
+                ["{}{}".format(" " * len(head), line) for line in lines[1:]])
+
+    def __repr__(self):
+        body = [self.__class__.__name__]
+        if self.transform is not None:
+            body += self._format_transform_repr(self.transform,
+                                                "Transform: ")
+        if self.target_transform is not None:
+            body += self._format_transform_repr(self.target_transform,
+                                                "Target transform: ")
+
+        return '\n'.join(body)
+
+
+class ChannelFusion:
+    '''
+    The class of Channel fusion data augmentation.
+    Args:
+
+    '''
+
+    def __init__(self, grouped_images):
+        self.grouped_images = grouped_images
+
+    def __call__(self, img, target):
+        img = np.asarray(img)
+        channels = list()
+        channels.append(np.split(img, 3, -1))
+        two_other_img = random.choices(self.grouped_images[target], k=2)
+        for toi in two_other_img:
+            channels.append(np.split(toi, 3, -1))
+        chosen_channels = [np.squeeze(x) for x in random.choices(channels, k=3)]
+
+        random.shuffle(chosen_channels)
+        new_img = np.stack(chosen_channels, axis=-1)
+
+        new_img = Image.fromarray(new_img)
+        return new_img
+
+
+class My_CIFAR10(VisionDataset):
+    """`CIFAR10 <https://www.cs.toronto.edu/~kriz/cifar.html>`_ Dataset.
+
+    Args:
+        root (string): Root directory of dataset where directory
+            ``cifar-10-batches-py`` exists or will be saved to if download is set to True.
+        train (bool, optional): If True, creates dataset from training set, otherwise
+            creates from test set.
+        transform (callable, optional): A function/transform that takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        download (bool, optional): If true, downloads the dataset from the internet and
+            puts it in root directory. If dataset is already downloaded, it is not
+            downloaded again.
+
+    """
+    base_folder = 'cifar-10-batches-py'
+    url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+    filename = "cifar-10-python.tar.gz"
+    tgz_md5 = 'c58f30108f718f92721af3b95e74349a'
+    train_list = [
+        ['data_batch_1', 'c99cafc152244af753f735de768cd75f'],
+        ['data_batch_2', 'd4bba439e000b95fd0a9bffe97cbabec'],
+        ['data_batch_3', '54ebc095f3ab1f0389bbae665268c751'],
+        ['data_batch_4', '634d18415352ddfa80567beed471001a'],
+        ['data_batch_5', '482c414d41f54cd18b22e5b47cb7c3cb'],
+    ]
+
+    test_list = [
+        ['test_batch', '40351d587109b95175f43aff81a1287e'],
+    ]
+    meta = {
+        'filename': 'batches.meta',
+        'key': 'label_names',
+        'md5': '5ff9c542aee3614f3951f8cda6e48888',
+    }
+
+    num_classes = 10
+
+    def __init__(self, root, train=True, transform=None, target_transform=None, channel_fusion=False):
+
+        super(My_CIFAR10, self).__init__(root, transform=transform,
+                                         target_transform=target_transform)
+        self.channel_fusion = channel_fusion
+        self.train = train  # training set or test set
+
+        if self.train:
+            downloaded_list = self.train_list
+        else:
+            downloaded_list = self.test_list
+
+        self.data = []
+        self.targets = []
+
+        # now load the picked numpy arrays
+        for file_name, checksum in downloaded_list:
+            file_path = os.path.join(self.root, self.base_folder, file_name)
+            with open(file_path, 'rb') as f:
+                if sys.version_info[0] == 2:
+                    entry = pickle.load(f)
+                else:
+                    entry = pickle.load(f, encoding='latin1')
+                self.data.append(entry['data'])
+                if 'labels' in entry:
+                    self.targets.extend(entry['labels'])
+                else:
+                    self.targets.extend(entry['fine_labels'])
+
+        self.data = np.vstack(self.data).reshape(-1, 3, 32, 32)
+        self.data = self.data.transpose((0, 2, 3, 1))  # convert to HWC
+
+        self.combined_data = [[] for _ in range(self.num_classes)]
+        for i, l in zip(self.data, self.targets):
+            self.combined_data[l].append(i)
+        if self.train:
+            if channel_fusion:
+                self.transform_before = ChannelFusion(self.combined_data)
+            else:
+                self.transform_before = None
+        self._load_meta()
+
+    def _load_meta(self):
+        path = os.path.join(self.root, self.base_folder, self.meta['filename'])
+
+        with open(path, 'rb') as infile:
+            if sys.version_info[0] == 2:
+                data = pickle.load(infile)
+            else:
+                data = pickle.load(infile, encoding='latin1')
+            self.classes = data[self.meta['key']]
+        self.class_to_idx = {_class: i for i, _class in enumerate(self.classes)}
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        img, target = self.data[index], self.targets[index]
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(img)
+        if self.train:
+            if self.transform_before:
+                img = self.transform_before(img, target)
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        return img, target
+
+    def __len__(self):
+        return len(self.data)
+
+    def extra_repr(self):
+        return "Split: {}".format("Train" if self.train is True else "Test")
+
+
+class My_CIFAR100(My_CIFAR10):
+    """
+    `CIFAR100 <https://www.cs.toronto.edu/~kriz/cifar.html>`_ Dataset.
+
+    This is a subclass of the `CIFAR10` Dataset.
+    """
+    base_folder = 'cifar-100-python'
+    url = "https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz"
+    filename = "cifar-100-python.tar.gz"
+    tgz_md5 = 'eb9058c3a382ffc7106e4002c42a8d85'
+    train_list = [
+        ['train', '16019d7e3df5f24257cddd939b257f8d'],
+    ]
+
+    test_list = [
+        ['test', 'f0ef6b0ae62326f3e7ffdfab6717acfc'],
+    ]
+    meta = {
+        'filename': 'meta',
+        'key': 'fine_label_names',
+        'md5': '7973b15100ade9c7d40fb424638fde48',
+    }
+    num_classes = 100
+
+
+class My_SVHN(VisionDataset):
+    """`SVHN <http://ufldl.stanford.edu/housenumbers/>`_ Dataset.
+    Note: The SVHN dataset assigns the label `10` to the digit `0`. However, in this Dataset,
+    we assign the label `0` to the digit `0` to be compatible with PyTorch loss functions which
+    expect the class labels to be in the range `[0, C-1]`
+
+    Args:
+        root (string): Root directory of dataset where directory
+            ``SVHN`` exists.
+        split (string): One of {'train', 'test', 'extra'}.
+            Accordingly dataset is selected. 'extra' is Extra training set.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        download (bool, optional): If true, downloads the dataset from the internet and
+            puts it in root directory. If dataset is already downloaded, it is not
+            downloaded again.
+
+    """
+
+    split_list = {
+        'train': ["http://ufldl.stanford.edu/housenumbers/train_32x32.mat",
+                  "train_32x32.mat", "e26dedcc434d2e4c54c9b2d4a06d8373"],
+        'test': ["http://ufldl.stanford.edu/housenumbers/test_32x32.mat",
+                 "test_32x32.mat", "eb5a983be6a315427106f1b164d9cef3"],
+        'extra': ["http://ufldl.stanford.edu/housenumbers/extra_32x32.mat",
+                  "extra_32x32.mat", "a93ce644f1a588dc4d68dda5feec44a7"]}
+    num_classes = 10
+
+    def __init__(self, root, split='train', transform=None, target_transform=None, channel_fusion=False):
+        super(My_SVHN, self).__init__(root, transform=transform,
+                                      target_transform=target_transform)
+        self.url = self.split_list[split][0]
+        self.filename = self.split_list[split][1]
+        self.file_md5 = self.split_list[split][2]
+        self.channel_fusion = channel_fusion
+        self.split = split
+        # import here rather than at top of file because this is
+        # an optional dependency for torchvision
+        import scipy.io as sio
+
+        if split == 'train':
+            # reading(loading) mat file as array
+            train_loaded_mat = sio.loadmat(os.path.join(self.root, self.split_list['train'][1]))
+            extra_loaded_mat = sio.loadmat(os.path.join(self.root, self.split_list['extra'][1]))
+            self.data = np.concatenate([train_loaded_mat['X'], extra_loaded_mat['X']], axis=-1)
+            # loading from the .mat file gives an np array of type np.uint8
+            # converting to np.int64, so that we have a LongTensor after
+            # the conversion from the numpy array
+            # the squeeze is needed to obtain a 1D tensor
+            self.labels = np.concatenate([train_loaded_mat['y'], extra_loaded_mat['y']], axis=0).astype(
+                np.int64).squeeze()
+        else:
+            # reading(loading) mat file as array
+            loaded_mat = sio.loadmat(os.path.join(self.root, self.filename))
+
+            self.data = loaded_mat['X']
+            # loading from the .mat file gives an np array of type np.uint8
+            # converting to np.int64, so that we have a LongTensor after
+            # the conversion from the numpy array
+            # the squeeze is needed to obtain a 1D tensor
+            self.labels = loaded_mat['y'].astype(np.int64).squeeze()
+
+        # the svhn dataset assigns the class label "10" to the digit 0
+        # this makes it inconsistent with several loss functions
+        # which expect the class labels to be in the range [0, C-1]
+        np.place(self.labels, self.labels == 10, 0)
+        self.data = np.transpose(self.data, (3, 0, 1, 2))
+        self.combined_data = [[] for _ in range(self.num_classes)]
+        for i, l in zip(self.data, self.labels):
+            self.combined_data[l].append(i)
+        if split == 'train':
+            if channel_fusion:
+                self.transform_before = ChannelFusion(self.combined_data)
+            else:
+                self.transform_before = None
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        img, target = self.data[index], int(self.labels[index])
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(img)
+        if self.split == 'train' or self.split == 'extra':
+            if self.transform_before:
+                img = self.transform_before(img, target)
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.data)
+
+    def extra_repr(self):
+        return "Split: {split}".format(**self.__dict__)
---
+++ main.py
@@ -0,0 +1,266 @@
+'''Train CIFAR10 with PyTorch.'''
+import argparse
+import json
+import os
+
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torchvision.transforms as transforms
+
+from dataloader import My_CIFAR10, My_CIFAR100, My_SVHN
+from models import *
+from utils import progress_bar
+
+parser = argparse.ArgumentParser(description='Models for cifar10/cifar100/svhn')
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+parser.add_argument('--dataset', '-d', choices=['cifar10', 'cifar100', 'svhn'], default='cifar10')
+parser.add_argument('--channel_fusion', '-cm', action='store_true', default=False)
+parser.add_argument('--model', type=str,
+                    choices=
+                    [
+                        'VGG11', 'VGG13', 'VGG16', 'VGG19',
+                        'ResNet18', 'ResNet34', 'ResNet50', 'ResNet101', 'ResNet152',
+                        'PreActResNet18', 'PreActResNet34', 'PreActResNet50', 'PreActResNet101', 'PreActResNet152',
+                        'GoogLeNet',
+                        'DenseNet121', 'DenseNet161', 'DenseNet169', 'DenseNet201',
+                        'ResNeXt29_2x64d', 'ResNeXt29_4x64d', 'ResNeXt29_8x64d', 'ResNeXt29_32x4d',
+                        'MobileNet', 'MobileNetV2',
+                        'DPN26', 'DPN92',
+                        'ShuffleNetG2', 'ShuffleNetG3', 'ShuffleNetV2',
+                        'SENet18',
+                        'EfficientNetB0'
+                    ], required=True)
+
+args = parser.parse_args()
+
+print(args)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+training_loss = []
+eval_loss = []
+training_acc = []
+eval_acc = []
+
+# Data
+print('==> Preparing data.. {}'.format(args.dataset))
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+dataset = args.dataset
+
+if dataset == 'cifar10':
+    trainset = My_CIFAR10(root='../data', train=True, transform=transform_train, channel_fusion=args.channel_fusion)
+    testset = My_CIFAR10(root='../data', train=False, transform=transform_test)
+    num_classes = 10
+elif dataset == 'cifar100':
+    trainset = My_CIFAR100(root='../data', train=True, transform=transform_train, channel_fusion=args.channel_fusion)
+    testset = My_CIFAR100(root='../data', train=False, transform=transform_test)
+    num_classes = 100
+elif dataset == 'svhn':
+    num_classes = 10
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.50705882, 0.48666667, 0.44078431), (0.26745098, 0.25647059, 0.27607843)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.50705882, 0.48666667, 0.44078431), (0.26745098, 0.25647059, 0.27607843)),
+    ])
+    trainset = My_SVHN('../data/svhn', split='train', transform=transform_train, channel_fusion=args.channel_fusion)
+    testset = My_SVHN('../data/svhn', split='test', transform=transform_test)
+else:
+    raise ValueError('Unsupported dataset.')
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=8)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=8)
+
+# classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# Model
+model = args.model
+
+print('==> Building model {}..'.format(model))
+
+if 'VGG' in model:
+    net = VGG(model, num_classes)
+elif model == 'ResNet18':
+    net = ResNet18(num_classes)
+elif model == 'ResNet34':
+    net = ResNet34(num_classes)
+elif model == 'ResNet50':
+    net = ResNet50(num_classes)
+elif model == 'ResNet101':
+    net = ResNet101(num_classes)
+elif model == 'ResNet152':
+    net = ResNet152(num_classes)
+elif model == 'PreActResNet18':
+    net = PreActResNet18(num_classes)
+elif model == 'PreActResNet34':
+    net = PreActResNet34(num_classes)
+elif model == 'PreActResNet50':
+    net = PreActResNet50(num_classes)
+elif model == 'PreActResNet101':
+    net = PreActResNet101(num_classes)
+elif model == 'PreActResNet152':
+    net = PreActResNet152(num_classes)
+elif model == 'GoogLeNet':
+    net = GoogLeNet(num_classes)
+elif model == 'DenseNet121':
+    net = DenseNet121(num_classes)
+elif model == 'DenseNet161':
+    net = DenseNet161(num_classes)
+elif model == 'DenseNet169':
+    net = DenseNet169(num_classes)
+elif model == 'DenseNet201':
+    net = DenseNet201(num_classes)
+elif model == 'ResNeXt29_2x64d':
+    net = ResNeXt29_2x64d(num_classes)
+elif model == 'ResNeXt29_4x64d':
+    net = ResNeXt29_4x64d(num_classes)
+elif model == 'ResNeXt29_8x64d':
+    net = ResNeXt29_8x64d(num_classes)
+elif model == 'ResNeXt29_32x4d':
+    net = ResNeXt29_32x4d(num_classes)
+elif model == 'MobileNet':
+    net = MobileNet(num_classes)
+elif model == 'MobileNetV2':
+    net = MobileNetV2(num_classes)
+elif model == 'DPN26':
+    net = DPN26(num_classes)
+elif model == 'DPN92':
+    net = DPN92(num_classes)
+elif model == 'ShuffleNetG2':
+    net = ShuffleNetG2(num_classes)
+elif model == 'ShuffleNetG3':
+    net = ShuffleNetG3(num_classes)
+elif model == 'SENet18':
+    net = SENet18(num_classes)
+elif model == 'ShuffleNetV2':
+    net = ShuffleNetV2(1, num_classes)
+elif model == 'EfficientNetB0':
+    net = EfficientNetB0(num_classes)
+else:
+    raise ValueError('Unsupported Model: {}'.format(model))
+
+net = net.to(device)
+if device == 'cuda':
+    net = torch.nn.DataParallel(net)
+    cudnn.benchmark = True
+
+if args.resume:
+    # Load checkpoint.
+    print('==> Resuming from checkpoint..')
+    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+    checkpoint = torch.load('./checkpoint/ckpt.pth')
+    net.load_state_dict(checkpoint['net'])
+    best_acc = checkpoint['acc']
+    start_epoch = checkpoint['epoch']
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[150, 250, 300], gamma=0.1)
+
+
+# Training
+def train(epoch):
+    global training_loss, training_acc
+    scheduler.step(epoch)
+    print('\nEpoch: {} \n learning rate: {}'.format(epoch, scheduler.get_lr()))
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+        training_loss.append(train_loss / (batch_idx + 1))
+        training_acc.append(correct / total)
+
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                     % (train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+
+
+def eval(epoch):
+    global best_acc, eval_loss, eval_acc
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            eval_loss.append(test_loss / (batch_idx + 1))
+            eval_acc.append(correct / total)
+
+            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                         % (test_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+
+    # Save checkpoint.
+    acc = 100. * correct / total
+    if acc > best_acc:
+        print('Saving..')
+        state = {
+            'net': net.state_dict(),
+            'acc': acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint/{}/{}'.format(args.dataset, model)):
+            os.makedirs('checkpoint/{}/{}'.format(args.dataset, model))
+        torch.save(state,
+                   './checkpoint/{}/{}/ckpt_{}_{}_{}.pth'.format(args.dataset,
+                                                                 model,
+                                                                 args.channel_fusion,
+                                                                 args.lr,
+                                                                 args.resume))
+        best_acc = acc
+
+
+if __name__ == '__main__':
+    for epoch in range(start_epoch, start_epoch + 400):
+        train(epoch)
+        eval(epoch)
+    with open(os.path.join('checkpoint/{}/{}'.format(args.dataset,
+                                                     model),
+                           'acc_loss_{}_{}_{}.json'.format(args.channel_fusion,
+                                                           args.lr,
+                                                           args.resume)), 'w', encoding='utf8') as f:
+        json.dump({
+            'train_loss': training_loss,
+            'test_loss': eval_loss,
+            'train_acc': training_acc,
+            'eval_acc': eval_acc
+        }, f)
